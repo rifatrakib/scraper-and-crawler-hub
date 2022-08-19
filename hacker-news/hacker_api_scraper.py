ebc2d5685c01782a80018f7a91fb4b8b2894b51c
rifatrakib <Rakib.1508@outlook.com>
2022-08-19 17:18:41 +0600

    hacker api used to scrape hacker news data

---
+++ hacker-news/hacker_api_scraper.py
@@ -0,0 +1,15 @@
+import requests
+
+articles = []
+url = "https://hacker-news.firebaseio.com/v0"
+top_stories = requests.get(url + "/topstories.json").json()
+
+for story_id in top_stories:
+    story_url = f"{url}/item/{story_id}.json"
+    print(f"fetching: {story_url}")
+    r = requests.get(story_url)
+    story_dict = r.json()
+    articles.append(story_dict)
+
+for article in articles:
+    print(article)
